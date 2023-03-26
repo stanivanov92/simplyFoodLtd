64ee98614e386168d9ff62159deb6552ea1373d0
Stanislav Ivanov <stan_ivanov@icloud.com>
2023-03-26 18:32:05 +0100

    instantiate the initial structure of the project

---
+++ src/main/Menu.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Menu class which will hold the products to be displayed to customers
+ */
+public class Menu {
+
+    public Menu(){
+        System.out.println("We have created a new object of type Menu");
+    }
+}
